7bfa399aa8f258f26b9e031772f1390d72b02ff9
rutujabhavar <rutujabhavar95@gmail.com>
2026-07-16 14:53:02 +0530

    Resolve Team.js merge conflict

--- frontend/src/landing_page/about/Team.js
+++ frontend/src/landing_page/about/Team.js
@@ -4,7 +4,7 @@ function Team() {
   return (
     <div className="container">
       <div className="row p-3 mt-5 border-top">
-        <h1 className="text-center ">People</h1>
+        <h1 className="text-center">People</h1>
       </div>
 
       <div
@@ -14,26 +14,40 @@ function Team() {
         <div className="col-6 p-3 text-center">
           <img
             src="media/RutujaBhavar.png.jpeg"
-            alt="RUTUJA BHAVAR"
+            alt="Rutuja Bhavar"
             style={{ borderRadius: "100%", width: "50%" }}
           />
           <h4 className="mt-5">RUTUJA BHAVAR</h4>
           <h6>Founder</h6>
         </div>
+
         <div className="col-6 p-3">
           <p>
-            Rutuja bootstrapped and founded Zerodha-Stock Trading Platform in 2026 to overcome the
-            hurdles he faced during his decade long stint as a trader. Today,
-            Zerodha-Stock Trading Platform has changed the landscape of the Indian broking industry.
+            Rutuja bootstrapped and founded Zerodha-Stock Trading Platform in
+            2026 to overcome the hurdles faced during the journey of building a
+            modern stock trading platform.
           </p>
+
           <p>
-            She is a member of the SEBI Secondary Market Advisory Committee
-            (SMAC) and the Market Data Advisory Committee (MDAC).
+            Passionate about web development, MERN Stack, and building
+            user-friendly applications.
           </p>
-          <p>Playing basketball is his zen.</p>
+
+          <p>Playing basketball is her zen.</p>
+
           <p>
-            Connect on <a href="https://www.linkedin.com/in/rutujabhavar06/">LinkedIn</a> / <a href="https//github.com/rutujabhavar">GitHub"</a> /{" "}
-           
+            Connect on{" "}
+            <a href="https://www.linkedin.com/in/rutujabhavar06/">
+              LinkedIn
+            </a>{" "}
+            /{" "}
+            <a href="https://github.com/rutujabhavar">
+              GitHub
+            </a>{" "}
+            /{" "}
+            <a href="mailto:rutujabhavar95@gmail.com">
+              Gmail
+            </a>
           </p>
         </div>
       </div>
