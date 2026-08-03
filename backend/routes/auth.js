c0a58cb723d624e25d006e443be1020d9d9f92bf
rutujabhavar <rutujabhavar95@gmail.com>
2026-08-03 23:14:42 +0530

    Fix backend auth route

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -1,6 +1,6 @@
-import React, { useEffect, useState } from "react";
-import { Link } from "react-router-dom";
-import axios from "axios";
+//import React, { useEffect, useState } from "react";
+//import { Link } from "react-router-dom";
+//import axios from "axios";
 
 const Menu = () => {
   const [selectedMenu, setSelectedMenu] = useState(0);
